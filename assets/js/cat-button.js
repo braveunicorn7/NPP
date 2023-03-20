cb8c084eb5566ef3888cf7227e578c0ae2debf3d
braveunicorn7 <brave.unicorn0710@gmail.com>
2023-03-20 17:16:33 -0400

    demo complete

---
+++ assets/js/cat-button.js
@@ -0,0 +1,34 @@
+function mouseover1() {
+    document.getElementById("button1").hidden = false;
+}
+function mouseleave1() {
+    document.getElementById("button1").hidden = true;
+}
+
+function mouseover2() {
+    document.getElementById("button2").hidden = false;
+}
+function mouseleave2() {
+    document.getElementById("button2").hidden = true;
+}
+
+function mouseover3() {
+    document.getElementById("button3").hidden = false;
+}
+function mouseleave3() {
+    document.getElementById("button3").hidden = true;
+}
+
+function mouseover4() {
+    document.getElementById("button4").hidden = false;
+}
+function mouseleave4() {
+    document.getElementById("button4").hidden = true;
+}
+
+function mouseover5() {
+    document.getElementById("button5").hidden = false;
+}
+function mouseleave5() {
+    document.getElementById("button5").hidden = true;
+}
